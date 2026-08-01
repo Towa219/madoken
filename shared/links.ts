96f6a9fa766a7ef64af20f96ad4190d78f94da15
ai-towa <ai.towa0219@gmail.com>
2026-08-02 06:32:12 +0900

    v0.10.3: 支援リンクをBOOTHショップに設定

--- shared/links.ts
+++ shared/links.ts
@@ -5,8 +5,8 @@ export const SITE_URL = 'https://madoken.onrender.com';
 
 // 支援ページのURL。空のままなら「支援」欄は表示されない。
 // 例: 'https://ko-fi.com/xxxx' / 'https://xxxx.booth.pm/' / 'https://www.pixiv.net/fanbox/creator/xxxx'
-export const SUPPORT_URL = '';
-export const SUPPORT_LABEL = '開発を支援する';
+export const SUPPORT_URL = 'https://madoken.booth.pm/';
+export const SUPPORT_LABEL = 'BOOTHで開発を支援する';
 export const SUPPORT_NOTE =
   'いただいた支援は、サーバー代(有料プランにするとスリープが無くなります)と'
   + 'グラフィック・音の制作に使わせていただきます。';
--- shared/version.ts
+++ shared/version.ts
@@ -1,5 +1,5 @@
 // バージョン情報(更新時はここだけ書き換える)
 
-export const VERSION = '0.10.2';
-export const BUILD_DATE = '2026-08-02 09:10';
+export const VERSION = '0.10.3';
+export const BUILD_DATE = '2026-08-02 09:40';
 export const COPYRIGHT = 'Copyright © 2026 ゆりパパ';
